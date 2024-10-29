feb09b6c3fb49cf6483079d169d829b205003e42
vaibhavambastha <vaibhav.ambastha@gmail.com>
2024-10-29 01:40:01 -0700

    change dir

--- statemachine.sv
+++ statemachine.sv
@@ -17,10 +17,6 @@ module statemachine(input logic slow_clock, input logic resetb,
                     output logic load_dcard1, output logic load_dcard2, output logic load_dcard3,
                     output logic player_win_light, output logic dealer_win_light);
 
-// The code describing your state machine will go here.  Remember that
-// a state machine consists of next state logic, output logic, and the 
-// registers that hold the state.  You will want to review your notes from
-// CPEN 211 or equivalent if you have forgotten how to write a state machine.
 
 logic [2:0] present_state;
 //Finite state machine that determines sequential logic
